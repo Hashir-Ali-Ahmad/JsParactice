4dd47b757d6d90c1f8c90e09b293eb1b4e4ccd65
Hashir Ali Ahmad <hashir.47a@gmail.com>
2023-11-10 09:35:53 +0000

    Type of

---
+++ Code/typeof.js
@@ -0,0 +1,34 @@
+// Premitive datatypes
+let a = 'Hashir'
+console.log(typeof a);
+
+let b = 45
+console.log(typeof b);
+
+let c = true
+console.log(typeof c);
+
+let d = null
+console.log(typeof d);
+
+let e;
+console.log(typeof e);
+
+// Non premitive
+
+let f = function (a,b){
+    return a + b
+}
+
+console.log(typeof f);
+
+let arr = [1,2,3]
+
+console.log(typeof arr);
+
+let person = {
+    name : "hashir",
+    age : 18 ,
+    city : "RYK"
+}
+console.log(typeof person);
