f5fa64f6753145e4ab7d968d103756a33c534509
Hashir Ali Ahmad <hashir.47a@gmail.com>
2023-11-10 09:59:51 +0000

    Strings and methods

---
+++ Code/string.js
@@ -0,0 +1,16 @@
+let name = 'Hashir'
+let age = 18
+ console.log(`My name is ${name} and age is ${age}`);
+
+//  Methods
+ let st = "    helloworld"
+ console.log(st.toUpperCase());
+ console.log(st.length);
+ console.log(st.charAt(6));
+ console.log(st.replace('h','H'));
+ console.log(st.trim());
+ console.log(st.slice(4,9));
+ console.log(st.concat(st," hi"));
+ 
+ let sub = st.substring(4,9);
+ console.log(sub); 
